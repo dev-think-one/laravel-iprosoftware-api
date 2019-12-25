e6935546aab3955637baf88ff1e5d9cd64765f32
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2019-12-25 18:58:35 +0200

    added Special Offers Type Enum

---
+++ src/Enums/SpecialOfferType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Angecode\LaravelIproSoft\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self SpecificDates()
+ * @method static self LastMinute()
+ * @method static self EarlyBird()
+ */
+class SpecialOfferType extends Enum
+{
+}
