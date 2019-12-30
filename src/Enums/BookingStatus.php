3dc15da42845a755666ba805850e0588391af248
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2019-12-30 19:05:20 +0200

    added BookingStatus enum

---
+++ src/Enums/BookingStatus.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Angecode\LaravelIproSoft\Enums;
+
+use Spatie\Enum\Enum;
+
+class BookingStatus extends Enum
+{
+    public static function blockout(): BookingStatus
+    {
+        return new class() extends BookingStatus {
+            public function getValue(): string
+            {
+                return 'Blockout';
+            }
+            public function getIndex(): int
+            {
+                return 1;
+            }
+        };
+    }
+
+    public static function provisional(): BookingStatus
+    {
+        return new class() extends BookingStatus {
+            public function getValue(): string
+            {
+                return 'Provisional';
+            }
+            public function getIndex(): int
+            {
+                return 2;
+            }
+        };
+    }
+
+    public static function booking(): BookingStatus
+    {
+        return new class() extends BookingStatus {
+            public function getValue(): string
+            {
+                return 'Booking';
+            }
+            public function getIndex(): int
+            {
+                return 3;
+            }
+        };
+    }
+
+    public static function cancelled(): BookingStatus
+    {
+        return new class() extends BookingStatus {
+            public function getValue(): string
+            {
+                return 'Cancelled';
+            }
+            public function getIndex(): int
+            {
+                return 4;
+            }
+        };
+    }
+}
