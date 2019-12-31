19f1602f09721b10ae542a7ccdf4f61303439d66
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2019-12-31 14:44:38 +0200

    added enum insurancebreakage

---
+++ src/Enums/InsuranceBreakage.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Angecode\LaravelIproSoft\Enums;
+
+
+use Spatie\Enum\Enum;
+
+class InsuranceBreakage extends Enum
+{
+
+    public static function none(): InsuranceBreakage
+    {
+        return new class() extends InsuranceBreakage {
+            public function getValue(): string
+            {
+                return 'None';
+            }
+            public function getIndex(): int
+            {
+                return 0;
+            }
+        };
+    }
+
+    public static function insurance(): InsuranceBreakage
+    {
+        return new class() extends InsuranceBreakage {
+            public function getValue(): string
+            {
+                return 'Insurance';
+            }
+            public function getIndex(): int
+            {
+                return 1;
+            }
+        };
+    }
+
+    public static function breakagesDeposit(): InsuranceBreakage
+    {
+        return new class() extends InsuranceBreakage {
+            public function getValue(): string
+            {
+                return 'BreakagesDeposit';
+            }
+            public function getIndex(): int
+            {
+                return 2;
+            }
+        };
+    }
+}
+
