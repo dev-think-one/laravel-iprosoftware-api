de5ffb36e6b39aa5f3a034a08390a103293cae0f
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2020-01-05 17:28:40 +0200

    format Test classes

--- tests/Enums/InsuranceBreakageTest.php
+++ tests/Enums/InsuranceBreakageTest.php
@@ -2,11 +2,6 @@
 
 namespace Angecode\LaravelIproSoft\Tests\Entities;
 
-use Angecode\LaravelIproSoft\Entities\AllPropertyInfo;
-use Angecode\LaravelIproSoft\Entities\GeneralPropertyInfo;
-use Angecode\LaravelIproSoft\Entities\OwnerInfo;
-use Angecode\LaravelIproSoft\Entities\PropertyInfoSubData\PropertyDetails;
-use Angecode\LaravelIproSoft\Enums\BookingStatus;
 use Angecode\LaravelIproSoft\Enums\InsuranceBreakage;
 use Angecode\LaravelIproSoft\Tests\IproSoftwareTestCase;
 
